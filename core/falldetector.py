c5b616306080cf5d1af86c8d9e2bc92436f60f6f
cwlroda <crescendostriker@gmail.com>
2020-09-04 18:15:13 +0800

    Improved fall detection threshold

--- core/falldetector.py
+++ core/falldetector.py
@@ -22,13 +22,13 @@ class FallDetector:
                     (old_x, old_y, old_w, old_h, fall) = self.old_persons[ID]
 
                     if w_ >= 1.2*h_:
-                        if math.sqrt(pow(abs(x-old_x), 2)+pow((y-old_y), 2)) >= 0.7*math.sqrt(pow(old_w, 2)+pow(old_h, 2)):
+                        if fall:
+                            self.falls[ID] = (x_, y_, w_, h_)
+                        
+                        elif math.sqrt(pow(abs(x-old_x), 2)+pow((y-old_y), 2)) >= 0.5*math.sqrt(pow(old_w, 2)+pow(old_h, 2)):
                             LOG.info("FALL DETECTED")
                             self.falls[ID] = (x_, y_, w_, h_)
                             self.old_persons[ID] = (x, y, old_w, old_h, True)
-                        
-                        elif fall:
-                            self.falls[ID] = (x_, y_, w_, h_)
                     
                     elif framecount % int(fps) == 0:
                         self.old_persons[ID] = (x, y, w_, h_, False)
